04e7d626d5d43e5cd9c292ff0b5dc65390dcd119
Michel D. Medeiros <119738123+Alquimas@users.noreply.github.com>
2023-09-26 11:30:15 -0300

    Create logica.c

---
+++ logica.c
@@ -0,0 +1,29 @@
+int main()
+{
+    //pra ler o numero
+    numero = 0
+    while(tive coisa pra ler)
+    {
+        a = coisalida;
+        numero *= 10
+        numero += a;
+    }
+
+    // pra printar um número
+    if(x == 0) printa(x)
+    else
+    {
+        contador = 0
+        while(x > 0)
+        {
+            coloca x % 10 na pilha
+            incrementa o contador em 1
+            divide x por 10
+        }
+        while(contador > 0)
+        {
+            printa o topo da pilha
+            diminui o contador em 1
+        }
+    }
+}
