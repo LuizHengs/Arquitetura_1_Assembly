f9b4545eca3db1d52802a9173e3b9b07c880b0e1
luther5000 <129391077+luther5000@users.noreply.github.com>
2023-09-29 22:52:59 -0300

    Update bola1.asm
    n tem mais error, mas tem 14 warning da mesma coisa

--- bola1.asm
+++ bola1.asm
@@ -6,10 +6,10 @@ dados	segment
 mens1	db	'Digite um numero (para finalizar tecle ENTER):#'
 mens2	db	'Deixe de ser burro digite um numero!#'
 numeros db	128 dup(?)
-livre   dw	0	;pode ser que aki esse 0 n seja pra colocar 0, ent pode dar pau
-nota1	db	0
-nota2	db	0
-nota3	db	0
+livre   dw	3 dup(?)	;pode ser que aki esse 0 n seja pra colocar 0, ent pode dar pau
+nota1	db	3 dup(?)
+nota2	db	3 dup(?)
+nota3	db	3 dup(?)
 dados ends
 
 codigo	segment
@@ -24,6 +24,14 @@ teste	proc	far
 	call	limpa
 	mov	dx,0
 	call	pos
+	lea si,livre
+	mov [si],0
+	lea si,nota1
+	mov [si],0
+	lea si,nota2
+	mov [si],0
+	lea si,nota3
+	mov [si],0
 	lea	bx, mens1
 	lea	si, numeros
 	mov	cl,3d
@@ -45,33 +53,53 @@ bola1:  call tec ;chama rotina do teclado
         call converteAscii	;primeira alteração
         inc si
         dec cl
-        cmp [cl],0		;se cl == 0 vai pra parte de adicionar a nota
+        cmp cl,0d		;se cl == 0 vai pra parte de adicionar a nota
         jz mudaNota1		;segunda alteração 
         jmp bola1
+bola3: lea di,mens2
+       mov al,0dh
+       call video
+       mov al,0ah
+       call video
+bola4: mov al,[di]
+       cmp al,'#'
+       jz bola5
+       call video
+	inc	di
+	jmp	bola4
+bola5:	mov	al,0dh
+	call	video
+	mov	al,0ah
+	call	video
+	jmp	bola1        
 mudaNota1: lea si, nota1	
-	cmp [si],0
+	cmp [si],0d
 	jnz mudaNota2		;se nota1 não for 0, vai pro loop da nota2
 	lea di,livre		;n sei se tem q tratar o di antes ent pode dar pau aki
-	mov [si],[di]
+	mov ax,[di]
+	mov [si],ax
 	mov cl,3d
-	mov [di],0
+	mov [di],0d
 	call limpa
 	call pos
 	jmp bola1
+fim:	ret 	;fim ta aki so por causa do pau da distancia
 mudaNota2: lea si, nota2	
-	cmp [si],0
+	cmp [si],0d
 	jnz mudaNota3		;se nota2 não for 0, vai pro loop da nota3
 	lea di,livre		;n sei se tem q tratar o di antes ent pode dar pau aki
-	mov [si],[di]
+	mov ax,[di]
+	mov [si],ax
 	mov cl,3d
-	mov [di],0
+	mov [di],0d
 	call limpa
 	call pos
 	jmp bola1
 mudaNota3: lea si,nota3
 	lea di,livre
-	mov [si],[di]	;temos as 3 notas agora
-	mov [di],0
+	mov ax,[di]
+	mov [si],ax	;temos as 3 notas agora
+	mov [di],0d
 	call limpa	;daremos prosseguimento ao codigo de forma normal, agora faremos a soma
 	call pos
 	jmp calcula
@@ -79,37 +107,43 @@ calcula:
 	lea di,livre	;vamos deixar di fixo em livre, que é a variavel onde haverão os calculos
 	lea si,nota1
 	clc
-	add [di],[si]
+	mov ax, [di]	;tem q deixar em algum registrador pra guardar
+			;a soma
+	add ax,[si]
 	clc
 	lea si,nota2
-	add [di],[si]
+	add ax,[si]
 	clc
 	lea si,nota3
-	add [di],[si]	;agr em livre temos a soma das 3 notas
+	add ax,[si]	;agr em livre temos a soma das 3 notas
 	
+	mov [di],ax
 	mov ax,3d		
 	div [di]	;di continua apontando para livre
-	mov [si],[ah]	;duas coisas 1)si continua apontando para nota3, ideia é que o conteudo esteja
+	mov [si],ah	;duas coisas 1)si continua apontando para nota3, ideia é que o conteudo esteja
 			;sendo colocado em nota3 2)aqui estamos colocando o resto dentro de nota3
 			;a ideia eh que aki ja temos o valor da divisão e o resto, então 
 			;agr precisamos voltar os valores para ascii
-	mov cl,0
+	mov cl,0d
 botaNaPilha:		;aki a gente vai colocar na pilha os valores na ordem em que eles vão ser
 			;printados (por ser pilha ordem inversa no caso)
 	mov AX,10d
 	div [di]
-	push ah
+	xor bh,bh
+	mov bl,ah
+	push BX		;aqui a gente zera a parte d cima de bx pra poder so ter a parte baixa como significativa, dps vamos dar pop nisso e colocar só a parte baixa do pop onde nós queremos
 	inc cl	
-	cmp al,0	;se o quociente for 0, significa que essa foi a última divisão possível
+	cmp al,0d	;se o quociente for 0, significa que essa foi a última divisão possível
 	jz vamosPrintar
 	jmp botaNaPilha
 vamosPrintar:
-	cmp cl,0
+	cmp cl,0d
 	jz AntesDoFim	;os prints tao em al, so codar pensando em colocar os valores em al e chamar 
 			;o video
-	pop al
+	pop BX
 	clc
-	sum [al],30	;somando 30 pra voltar pra o valor em ascii antes do print
+	mov al,bl
+	add al,30d	;somando 30 pra voltar pra o valor em ascii antes do print
 	call video
 	dec cl
 	jmp vamosPrintar
@@ -137,23 +171,7 @@ caso2:
 	mov al,37h
 	call video
 	jmp fim
-bola3: lea di,mens2
-       mov al,0dh
-       call video
-       mov al,0ah
-       call video
-bola4: mov al,[di]
-       cmp al,'#'
-       jz bola5
-       call video
-	inc	di
-	jmp	bola4
-bola5:	mov	al,0dh
-	call	video
-	mov	al,0ah
-	call	video
-	jmp	bola1
-fim:	ret
+
 teste	endp
 
 video	proc	near
@@ -207,11 +225,16 @@ converteAscii proc near
       push DI
       clc			;limpando carry
       lea di,livre		;usando di pq o si ta com o valor que o usuário acabou de inserir
-      mul [di],0ah		;a cada vez q tem um novo número tem que multiplicar o existente por 10
+      xor ah,ah
+      mov al,10d
+      mul [di]		;a cada vez q tem um novo número tem que multiplicar o existente por 10
+      mov [di],ax
       clc
-      sub [si],30		;subtraindo por 30 pra pegar o binario
+      sub [si],30d		;subtraindo por 30 pra pegar o binario
       clc
-      sum [di],[si]
+      mov ax,[di]
+      add ax,[si]
+      mov [di],ax
       pop DI
       pop DX
       pop CX
